44887781ace6a5a7c19d2828e8c53bbcfe1e6fbf
Wojtek2810 <75879048+Wojtek2810@users.noreply.github.com>
2023-10-19 14:04:10 +0200

    Rekurencja z funkcją flagi

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from tkinter import *
-from datetime import datetime
+from datetime import datetime, timedelta
 import math
 import csv
+import time
+from random import randint
 
 # ---------------------------- CONSTANTS ------------------------------- #
 WHITE = "#f8f8f8"
@@ -12,9 +14,25 @@ DARK_GREY = "#929AAB"
 FONT_NAME = "Courier"
 
 # ---------------------------- VARIABLES ------------------------------- #
-status = "off"
+status = "work"
 
 mode = "now"
+
+timer_r = None
+
+clock_running = False
+
+
+# ----------------------------- RUN ONCE DECORATOR ------------------------------- #
+def run_once(func):
+    def wrapper(*args, **kwargs):
+        if not wrapper.has_run:
+            if clock_running is False:
+                wrapper.has_run = True
+            return func(*args, **kwargs)
+    wrapper.has_run = False
+    return wrapper
+
 
 # ---------------------------------- SAVE DAY ------------------------------------ #
 def save_day():
@@ -23,9 +41,80 @@ def save_day():
         writer = csv.writer(file)
         writer.writerow([now])
 
+
+# ---------------------------------- SAVE DAY ------------------------------------ #
+def save_time():
+    now = datetime.now()
+    with open("time.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([now])
+
+
 # ----------------------------- COUNTING MECHANISM ------------------------------- #
-def counting():
+@run_once
+def counting(seconds_overall):
+    if clock_running:
+        global timer_r
+        godziny = seconds_overall // 60 // 60 % 60
+        if godziny < 10:
+            godziny = f"0{godziny}"
+        minuty = seconds_overall // 60 % 60
+        if minuty < 10:
+            minuty = f"0{minuty}"
+        sekundy = seconds_overall % 60
+        if sekundy < 10:
+            sekundy = f"0{sekundy}"
+
+        canvas.itemconfig(timer_text, text=f"{godziny}:{minuty}")
+        canvas.itemconfig(seconds_text, text=sekundy)
+
+        if seconds_overall % 4 == 0 or seconds_overall % 4 == 1 or seconds_overall % 4 == 2:
+            canvas.itemconfig(status_text, text=status)
+
+        if seconds_overall % 4 == 3:
+            canvas.itemconfig(status_text, text="")
+
+        if seconds_overall % 4 == 0 or seconds_overall % 4 == 3 or seconds_overall % 4 == 2:
+            canvas.itemconfig(mode_text, text=mode)
+
+        if seconds_overall % 4 == 1:
+            canvas.itemconfig(mode_text, text="")
+
+        timer_r = window.after(1000, counting, seconds_overall + 1)
+
+
+# --------------------------------- START/STOP ----------------------------------- #
+def start_stop():
     global status
+    global clock_running
+
+    save_time()
+
+    status = "work"
+
+    clock_running = False
+    initiation()
+
+
+# ----------------------------------- MODE -------------------------------------- #
+def change_mode():
+    global mode
+    global clock_running
+
+    if mode == "now":
+        mode = "day"
+
+    elif mode == "day":
+        mode = "now"
+
+    clock_running = False
+    initiation()
+
+
+# ---------------------------- INITIATION MECHANISM ------------------------------ #
+def initiation():
+    global status
+    global clock_running
 
     with open("time.csv", mode='r') as file:
         reader = csv.reader(file)
@@ -36,25 +125,60 @@ def counting():
     now = datetime.now()
     if not data or data[0][:10] != now.strftime("%Y-%m-%d"):
         save_day()
-    elif data[0][:10] == now.strftime("%Y-%m-%d"):
+
+    # Wczytana data jako string
+    data_string_last = data[-1]
+    # Format daty w podanym stringu
+    date_format = "%Y-%m-%d %H:%M:%S.%f"
+    # Utwórz obiekt datetime z podanej daty
+    parsed_date_last = datetime.strptime(data_string_last, date_format)
+
+    if mode == "now":
         if len(data) % 2 != 0:
-            #Wczytana data jako string
-            data_string = data[-1]
-            # Format daty w podanym stringu
-            date_format = "%Y-%m-%d %H:%M:%S.%f"
-            # Utwórz obiekt datetime z podanej daty
-            parsed_date = datetime.strptime(data_string, date_format)
             #Czas który upłynął
-            current_work_time = now - parsed_date
-            minuty = int(current_work_time.total_seconds() // 60 % 60)
-            if minuty < 10:
-                minuty = f"0{minuty}"
-            sekundy = int(current_work_time.total_seconds() % 60)
-            if sekundy < 10:
-                sekundy = f"0{sekundy}"
-            canvas.itemconfig(timer_text, text=f"{minuty}:{sekundy}")
+            current_work_time = now - parsed_date_last
         else:
-            pass
+            status = "break"
+            data_string_second_last = data[-2]
+            parsed_date_second_last = datetime.strptime(data_string_second_last, date_format)
+            current_work_time = parsed_date_last - parsed_date_second_last
+    else:
+        start_moment = []
+        end_moment = []
+        for i in range(len(data)):
+            if i % 2 == 0:
+                start_moment.append(datetime.strptime(data[i], date_format))
+            else:
+                end_moment.append(datetime.strptime(data[i], date_format))
+
+        if len(data) % 2 != 0:
+            end_moment.append(now)
+
+        current_work_time = timedelta()
+        for i in range(len(start_moment)):
+            current_work_time += end_moment[i] - start_moment[i]
+
+    godziny = int(current_work_time.total_seconds() // 60 // 60 % 60)
+    if godziny < 10:
+        godziny = f"0{godziny}"
+    minuty = int(current_work_time.total_seconds() //60 % 60)
+    if minuty < 10:
+        minuty = f"0{minuty}"
+    sekundy = int(current_work_time.total_seconds() % 60)
+    if sekundy < 10:
+        sekundy = f"0{sekundy}"
+
+    canvas.itemconfig(timer_text, text=f"{godziny}:{minuty}")
+    canvas.itemconfig(seconds_text, text=sekundy)
+    seconds_overall = int(current_work_time.total_seconds())
+
+    canvas.itemconfig(status_text, text=status)
+    canvas.itemconfig(mode_text, text=mode)
+
+    if len(data) % 2 != 0:
+        clock_running = True
+        counting(seconds_overall)
+
 
 
 
@@ -69,23 +193,7 @@ def counting():
 
 
 
-    # count_min = math.floor(count / 60)
-    # count_sec = count % 60
-    # if count_sec < 10:
-    #     count_sec = f'0{count_sec}'
-    # if count_min < 10:
-    #     count_min = f'0{count_min}'
-    # canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
-    # if count > 0:
-    #     global timer_r
-    #     timer_r = window.after(1000, counting)
-    # else:
-    #     start_timer()
-    #     text = ""
-    #     work_sessions = math.floor(reps/2)
-    #     for _ in range(work_sessions):
-    #         text += "✔"
-    #     check_mark.config(text=text)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -107,19 +215,20 @@ canvas = Canvas(width=395, height=675, bg=WHITE, highlightthickness=0)
 watch_img = PhotoImage(file="watch_img.png")
 canvas.create_image(197, 337, image=watch_img)
 timer_text = canvas.create_text(197, 308, text="00:00", fill=GREY, font=(FONT_NAME, 45, "bold"))
+seconds_text = canvas.create_text(263, 350, text="00", fill=GREY, font=(FONT_NAME, 23, "bold"))
 status_text = canvas.create_text(120, 200, text="work", fill=GREY, font=(FONT_NAME, 15, "bold"))
 mode_text = canvas.create_text(265, 200, text="now", fill=GREY, font=(FONT_NAME, 15, "bold"))
 canvas.grid(column=0, row=3, pady=25)
 
 # Buttons
 start_button = PhotoImage(file="button.png")
-button_1 = Button(canvas, image=start_button, highlightthickness=0)
+button_1 = Button(canvas, image=start_button, highlightthickness=0, command=start_stop)
 button_1_window = canvas.create_window(120, 452, anchor=CENTER, window=button_1)
 
-button_2 = Button(canvas, image=start_button, highlightthickness=0)
+button_2 = Button(canvas, image=start_button, highlightthickness=0, command=change_mode)
 button_2_window = canvas.create_window(265, 452, anchor=CENTER, window=button_2)
 
-counting()
+initiation()
 
 
 
